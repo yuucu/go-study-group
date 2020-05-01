36a0242ff9d3a87cda10fdcec243f7d37085c2dd
yuucu <yuucu.sub@gmail.com>
2020-05-01 22:55:06 +0900

    solved q1

--- chapter3/kadai1.go
+++ chapter3/kadai1.go
@@ -8,3 +8,19 @@ type Kadai1 struct {
 	id   int
 	name string
 }
+
+func (k1 Kadai1) ID() int {
+	return k1.id
+}
+
+func (k1 *Kadai1) SetID(id int) {
+	k1.id = id
+}
+
+func (k1 Kadai1) Name() string {
+	return k1.name
+}
+
+func (k1 *Kadai1) SetName(name string) {
+	k1.name = name
+}
